136f4db5084656c4b5c094df5c2d6116aec4e038
Kirill Akulich <kirillakulich0@gmail.com>
2021-09-23 22:51:37 +0300

    dynamic lines added

--- public/App.js
+++ public/App.js
@@ -43,6 +43,24 @@ var IssueFilter = /*#__PURE__*/function (_React$Component) {
   return IssueFilter;
 }(React.Component);
 
+var issues = [{
+  id: 1,
+  status: 'New',
+  owner: 'Ravan',
+  effort: 5,
+  created: new Date('2018-08-15'),
+  due: undefined,
+  title: 'Error in console when clicking Add'
+}, {
+  id: 2,
+  status: 'Assigned',
+  owner: 'Eddie',
+  effort: 14,
+  created: new Date('2018-08-16'),
+  due: new Date('2018-08-30'),
+  title: 'Missing bottom border on panel'
+}];
+
 var IssueRow = /*#__PURE__*/function (_React$Component2) {
   _inherits(IssueRow, _React$Component2);
 
@@ -57,12 +75,8 @@ var IssueRow = /*#__PURE__*/function (_React$Component2) {
   _createClass(IssueRow, [{
     key: "render",
     value: function render() {
-      var style = this.props.rowStyle;
-      return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
-        style: style
-      }, this.props.issue_id), /*#__PURE__*/React.createElement("td", {
-        style: style
-      }, this.props.issue_title));
+      var issue = this.props.issue;
+      return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, issue.id), /*#__PURE__*/React.createElement("td", null, issue.status), /*#__PURE__*/React.createElement("td", null, issue.owner), /*#__PURE__*/React.createElement("td", null, issue.created.toDateString()), /*#__PURE__*/React.createElement("td", null, issue.effort), /*#__PURE__*/React.createElement("td", null, issue.due ? issue.due.toDateString() : ''), /*#__PURE__*/React.createElement("td", null, issue.title));
     }
   }]);
 
@@ -83,19 +97,15 @@ var IssueTable = /*#__PURE__*/function (_React$Component3) {
   _createClass(IssueTable, [{
     key: "render",
     value: function render() {
-      var rowStyle = {
-        border: "1px solid silver",
-        padding: 4
-      };
-      return /*#__PURE__*/React.createElement("table", null, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "ID"), /*#__PURE__*/React.createElement("th", null, "Title"))), /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement(IssueRow, {
-        rowStyle: rowStyle,
-        issue_id: 1,
-        issue_title: "Error in console when clicking Add"
-      }), /*#__PURE__*/React.createElement(IssueRow, {
-        rowStyle: rowStyle,
-        issue_id: 2,
-        issue_title: "Missing bottom border on panel"
-      })));
+      var issueRows = issues.map(function (issue) {
+        return /*#__PURE__*/React.createElement(IssueRow, {
+          key: issue.id,
+          issue: issue
+        });
+      });
+      return /*#__PURE__*/React.createElement("table", {
+        className: "bordered-table"
+      }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "ID"), /*#__PURE__*/React.createElement("th", null, "Status"), /*#__PURE__*/React.createElement("th", null, "Owner"), /*#__PURE__*/React.createElement("th", null, "Created"), /*#__PURE__*/React.createElement("th", null, "Effort"), /*#__PURE__*/React.createElement("th", null, "Due Date"), /*#__PURE__*/React.createElement("th", null, "Title"))), /*#__PURE__*/React.createElement("tbody", null, issueRows));
     }
   }]);
 
--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,32 @@ class IssueFilter extends React.Component {
   }
 }
 
+const issues = [
+  {
+    id: 1, status: 'New', owner: 'Ravan', effort: 5,
+    created: new Date('2018-08-15'), due: undefined,
+    title: 'Error in console when clicking Add',
+  },
+  {
+    id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
+    created: new Date('2018-08-16'), due: new Date('2018-08-30'),
+    title: 'Missing bottom border on panel',
+  },
+];
+
+
 class IssueRow extends React.Component {
   render() {
-    const style = this.props.rowStyle;
+    const issue = this.props.issue;
     return (
       <tr>
-        <td style={style}>{this.props.issue_id}</td>
-        <td style={style}>{this.props.issue_title}</td>
+        <td>{issue.id}</td>
+        <td>{issue.status}</td>
+        <td>{issue.owner}</td>
+        <td>{issue.created.toDateString()}</td>
+        <td>{issue.effort}</td>
+        <td>{issue.due ? issue.due.toDateString() : ''}</td>
+        <td>{issue.title}</td>
       </tr>
     );
   }
@@ -20,20 +39,24 @@ class IssueRow extends React.Component {
 
 class IssueTable extends React.Component {
   render() {
-    const rowStyle = {border: "1px solid silver", padding: 4};
+    const issueRows = issues.map(issue =>
+      <IssueRow key={issue.id} issue={issue} />
+    );
     return (
-      <table>
+      <table className="bordered-table">
         <thead>
           <tr>
             <th>ID</th>
+            <th>Status</th>
+            <th>Owner</th>
+            <th>Created</th>
+            <th>Effort</th>
+            <th>Due Date</th>
             <th>Title</th>
           </tr>
         </thead>
         <tbody>
-        <IssueRow rowStyle={rowStyle} issue_id={1}
-            issue_title="Error in console when clicking Add" />
-          <IssueRow rowStyle={rowStyle} issue_id={2}
-            issue_title="Missing bottom border on panel" />
+          {issueRows}
         </tbody>
       </table>
     );
