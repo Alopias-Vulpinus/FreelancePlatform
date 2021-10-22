da367e71781777fd9a12ad6bf43344d60a4cf464
Kirill Akulich <kirillakulich0@gmail.com>
2021-10-22 12:30:30 +0300

    command fixed

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ app.use(express.json({ extended: true }))
 
 app.use('/api/auth' , require('./routes/auth.routes'))
 app.use('/t', require('./routes/redirect.routes'))
-app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-app.get('*', (req, res) => {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
+
 const PORT = config.get('port') || 5000
 
 async function start() {
