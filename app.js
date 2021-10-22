3f1ebbb4a5300e8a1fda0e941f74e027cadc64c3
Kirill Akulich <kirillakulich0@gmail.com>
2021-10-22 13:18:34 +0300

    deploy fixed

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ app.use(express.json({ extended: true }))
 
 app.use('/api/auth' , require('./routes/auth.routes'))
 app.use('/t', require('./routes/redirect.routes'))
-
+// uncomment it before deploy
+/*
   app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
-
+*/
 const PORT = config.get('port') || 5000
 
 async function start() {
