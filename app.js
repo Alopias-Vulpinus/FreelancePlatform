02dac9587d5d7fd75438b8e45f6e1d457a445286
Kirill Akulich <kirillakulich0@gmail.com>
2021-10-22 10:41:46 +0300

    remove links

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const app = express()
 
 app.use(express.json({ extended: true }))
 
-app.use('/api/auth', require('./routes/auth.routes'))
-app.use('/api/link', require('./routes/link.routes'))
+app.use('/api/auth' , require('./routes/auth.routes'))
 app.use('/t', require('./routes/redirect.routes'))
 
 if (process.env.NODE_ENV === 'production') {
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,24 +1,11 @@
 import React from 'react'
 import {Switch, Route, Redirect} from 'react-router-dom'
-import {LinksPage} from './pages/LinksPage'
-import {CreatePage} from './pages/CreatePage'
-import {DetailPage} from './pages/DetailPage'
 import {AuthPage} from './pages/AuthPage'
 
 export const useRoutes = isAuthenticated => {
   if (isAuthenticated) {
     return (
       <Switch>
-        <Route path="/links" exact>
-          <LinksPage />
-        </Route>
-        <Route path="/create" exact>
-          <CreatePage />
-        </Route>
-        <Route path="/detail/:id">
-          <DetailPage />
-        </Route>
-        <Redirect to="/create" />
       </Switch>
     )
   }
