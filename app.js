a0625952534080cbdc752cc2a9d878a6a819c6e2
Kirill Akulich <kirillakulich0@gmail.com>
2021-10-22 11:02:44 +0300

    npm start fixed

--- app.js
+++ app.js
@@ -9,15 +9,11 @@ app.use(express.json({ extended: true }))
 
 app.use('/api/auth' , require('./routes/auth.routes'))
 app.use('/t', require('./routes/redirect.routes'))
-/*
-if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-  app.get('*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
-}*/
-
 const PORT = config.get('port') || 5000
 
 async function start() {
