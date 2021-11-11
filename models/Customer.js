9dfaf52c18cdd7d3d01a01084d16a798453bf641
Kiryl Akulich <62598669+KirylAkulich1@users.noreply.github.com>
2021-11-11 03:34:11 +0300

    Revert "models added"

--- models/Customer.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model, Types} = require('mongoose')
-
-const schema = new Schema({
-    user_data_id:{type:String},
-    tasks_ids:{type:[String]},
-    notification_ids:{type:[String]}
-  })
-  
-module.exports = model('Customer', schema)
--- models/Status.js
+++
@@ -1,8 +0,0 @@
-const {Schema, model, Types} = require('mongoose')
-
-const schema = new Schema({
-    name:{type:String, required: true},
-    numeric_value:{type:Number},
-  })
-  
-  module.exports = model('Status', schema)
